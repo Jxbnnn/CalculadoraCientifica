64bb7c078ac6be5e3ee6421bada3221da4316e2f
Benjamin <benjaminurrea14@gmail.com>
2025-05-01 03:34:54 -0400

    Segundo commit

---
+++ src/main/java/org/example/Calculadoracientifica.java
@@ -0,0 +1,18 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class Calculadoracientifica {
+    static class Menu {
+        void mostrarPrincipal() {
+            System.out.println("\n=== CALCULADORA ===");
+            System.out.println("1. Operaciones básicas");
+            System.out.println("2. Ecuación cuadrática");
+            System.out.println("3. Figuras geométricas");
+            System.out.println("4. Sistema de ecuaciones");
+            System.out.println("5. Ecuación de recta");
+            System.out.println("6. Salir");
+            System.out.print("Elige opción: ");
+        }
+    }
+}
